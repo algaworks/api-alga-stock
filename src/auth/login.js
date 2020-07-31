5cc30c1f3ba9833b7229ba2d9f02a12c73e1dbf1
Daniel Bonifacio <contato@danielbonifacio.com.br>
2020-07-31 11:04:38 -0300

    fix login

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,10 +1,25 @@
 module.exports = function login (user, pass) {
-  if (user === process.env.USER && pass === process.env.PASS) {
+  if (
+    user === process.env.ADMIN_USER &&
+    pass === process.env.ADMIN_PASS
+  ) {
     return {
       id: 1,
-      token: process.env.TOKEN,
-      user: process.env.USER,
-      email: process.env.EMAIL
+      token: process.env.ADMIN_TOKEN,
+      user: process.env.ADMIN_USER,
+      email: process.env.ADMIN_EMAIL
+    }
+  }
+  
+  if (
+    user === process.env.CUSTOMER_USER &&
+    pass === process.env.CUSTOMER_PASS
+  ) {
+    return {
+      id: 2,
+      token: process.env.CUSTOMER_TOKEN,
+      user: process.env.CUSTOMER_USER,
+      email: process.env.CUSTOMER_EMAIL
     }
   }
 
